3f5470fc8d49b8ad9138509a9989aacdbdd13201
Ashen Indeewara <a.indeewara911@gmail.com>
2025-11-20 13:50:10 +0530

    Fix CORS origin URL for frontend application

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://backend-blog-navy.vercel.app"],
+      "https://fontend-blog.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"] // optional
   })
 )
